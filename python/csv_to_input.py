8696461445f74c2bbe72ccf238534674012890a7
I am the Walrus <stag@Wal.Ross>
2017-04-01 01:25:26 +1100

    midi to csv

---
+++ python/csv_to_input.py
@@ -0,0 +1,14 @@
+def convert(f):
+    content = open(f).readlines()
+    print content
+
+
+#2, 0, Start_track
+#2, 0, MIDI_port, 0
+#2, 0, Title_t, "Violin"
+#2, 0, Program_c, 0, 40
+#2, 0, Control_c, 0, 7, 100
+#2, 0, Control_c, 0, 10, 90
+#2, 60, Channel_aftertouch_c, 0, 70
+#2, 180, Note_on_c, 0, 79, 105
+#2, 262, Note_on_c, 0, 79, 0
