d10e909ef1dadaf9ba3778335ba1e481e8c9a825
Axel Nyström <mat04any@student.lth.se>
2017-03-31 16:06:05 +0200

    started work on midi csv data processing

---
+++ Data Processing/data2csv.py
@@ -0,0 +1,48 @@
+# data = open('test.txt', 'r').read()
+
+# for line in data:
+# 	print line
+
+
+
+
+t = 0
+# Track, Time, Type, Channel, Note, Velocity
+# 2, 960, Note_on_c, 1, 81, 81
+
+
+
+
+def processChunk(chunk):
+	global t
+	track = "2"
+	dt = (ord(chunk[0])<<8) + (ord(chunk[1]))
+	t += dt
+	#print "chunk[0]="+str(ord(chunk[0]))+", chunk[1]="+str(ord(chunk[1]))
+	#print "dt = "+str(dt)
+	time = str(t)
+	trackType = "-"
+	if ord(chunk[2])&1 == 1:
+		trackType = "Note_on_c"
+	else:
+		trackType = "Note_off_c"
+	channel = "1"
+	note = str(ord(chunk[3]))
+	velocity = str(ord(chunk[4]))
+
+	return ",".join((track,time,trackType,channel,note,velocity))
+
+
+
+with open('data.txt') as f:
+	data = f.read()
+	n = len(data)
+	pos = 0
+	step = 5
+	while pos <= n-step:
+
+		chunk = data[pos:pos+step]
+		pos += step
+
+		print processChunk(chunk)
+		
