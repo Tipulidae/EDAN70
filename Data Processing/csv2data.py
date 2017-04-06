fc7179acf351e508125a4572934942166d8432dc
Axel Nyström <mat04any@student.lth.se>
2017-04-06 11:14:23 +0200

    changed to use only 128 characters instead of 256

--- Data Processing/csv2data.py
+++ Data Processing/csv2data.py
@@ -2,35 +2,51 @@ import sys
 from os import listdir
 from os.path import isfile, join
 
+
+"""
 t = 0
 realTempo = 500000
 tempo = 500000
-# Track, Time, Type, Channel, Note, Velocity
-# 2, 960, Note_on_c, 1, 81, 81
-# In header, assuming division = 480..
-
 previousClock = 0
 deltaT = 0
+"""
 
-
-
-def parseNoteOnOff(note,isOn):
-	return parseTime(int(note[1])) + parseType(isOn) + parseNote(note[4]) + parseVelocity(note[5])
-
-def parseTime(time):
-	global previousClock
-	global tempo
-	global realTempo
-	global deltaT
+class Tempo:
 	
-	dt = deltaT + (time-previousClock)*tempo/realTempo
-	previousClock = time
-	deltaT = 0
+	def __init__(self):
+		self.realTempo = 500000
+		self.tempo = 500000
+		self.previousClock = 0
+		self.deltaT = 0
+		
+	
+	def timeSinceLastEvent(self,time):
+		dt = self.deltaT + (time-self.previousClock)*self.tempo/self.realTempo
+		self.previousClock = time
+		self.deltaT = 0
+		return dt
 
-	if dt < 0 or dt > 131071:
+	def changeTempoEvent(self, time, newTempo):
+		self.deltaT += (time-self.previousClock)*self.tempo/self.realTempo
+		self.tempo = newTempo
+		self.previousClock = time
+	
+	def reset(self):
+		self.previousClock = 0
+		self.deltaT = 2880
+
+
+
+def parseNoteOnOff(t,note,isOn):
+	return parseTime(t,int(note[1])) + parseType(isOn) + parseNote(note[4]) + parseVelocity(note[5])
+
+def parseTime(t,time):
+	dt = t.timeSinceLastEvent(time)
+
+	if dt < 0 or dt > 32767:
 		print "WARNING! Something went wrong with dt! dt = "+str(dt)+". Proceeding anyway..."
-	msb = (dt>>8)&127
-	lsb = dt&255
+	msb = (dt>>7)&127
+	lsb = dt&127
 	#print "msb = "+str(msb)+", lsb = "+str(lsb)
 	return chr(msb)+chr(lsb)
 
@@ -41,27 +57,17 @@ def parseType(isOn):
 		return chr(0)
 
 def parseNote(note):
-	return chr(int(note)&255)
+	return chr(int(note)&127)
 
 def parseVelocity(vel):
-	return chr(int(vel)&255)
+	return chr(int(vel)&127)
 
-def parseTempoChange(time, newtempo):
-	global deltaT
-	global tempo
-	global previousClock
-	global realTempo
-	deltaT += (time-previousClock)*tempo/realTempo
-	tempo = newtempo
-	previousClock = time
+def parseTempoChange(t, time, newTempo):
+	t.changeTempoEvent(time,newTempo)
 
 
-def parseAllContentInFile(content):
-	global previousClock
-	global deltaT
-
-	previousClock = 0
-	deltaT = 2880
+def parseAllContentInFile(t, content):
+	t.reset()
 	data = ""
 	
 	for line in content:
@@ -70,20 +76,18 @@ def parseAllContentInFile(content):
 			continue
 		lineType = items[2]
 
-		
-		f[
 
 		if lineType == 'note_on_c':
-			output = parseNoteOnOff(items,True)
+			output = parseNoteOnOff(t,items,True)
 			#print str(items)+" -> "+output + ", length = "+str(len(output))
 			data += output
 		elif lineType == 'note_off_c':
-			output = parseNoteOnOff(items,False)
+			output = parseNoteOnOff(t,items,False)
 			#print str(items)+" -> "+output + ", length = "+str(len(output))
 			data += output
 		elif lineType == 'tempo':
 			#tempo = int(items[3])
-			parseTempoChange(int(items[1]),int(items[3]))
+			parseTempoChange(t,int(items[1]),int(items[3]))
 			#print "tempo was changed to "+str(tempo)+"!"
 		elif lineType[-2:] != '_c':
 			continue
@@ -99,10 +103,11 @@ if __name__ == '__main__':
 	
 	
 	data = ""
+	t = Tempo()
 	files = [f for f in listdir(PATH) if isfile(join(PATH,f))]
 	for file in files:
 		with open(join(PATH,file),'r') as f:
-			data += parseAllContentInFile(f.readlines())
+			data += parseAllContentInFile(t, f.readlines())
 	
 	
 	with open(datafile,'w') as f:
