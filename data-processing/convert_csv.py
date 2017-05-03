0d395fc19293f0a5a02e15c28b16b63ae86fd0b9
I am the Walrus <stag@Wal.Ross>
2017-05-03 23:47:09 +1000

    output after 50ep

--- data-processing/convert_csv.py
+++ data-processing/convert_csv.py
@@ -2,8 +2,8 @@ from os import listdir
 from os.path import isfile, join
 import subprocess
 
-PATH = "/home/stag/EDAN70/midi"
-EXPORT_PATH = "/home/stag/EDAN70/csv"
+PATH = "midi"
+EXPORT_PATH = "csv"
 
 
 files = [f for f in listdir(PATH) if isfile(join(PATH, f))]
