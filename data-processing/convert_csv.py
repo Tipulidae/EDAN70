a2f6b8117066606e7603cf4b73fdf04c6ec2cedc
I am the Walrus <stag@Wal.Ross>
2017-05-04 20:18:00 +1000

    sample and bug in csv2data2

--- data-processing/convert_csv.py
+++ data-processing/convert_csv.py
@@ -7,6 +7,11 @@ EXPORT_PATH = "csv"
 
 files = [f for f in listdir(PATH) if isfile(join(PATH, f))]
 
+temp = 0
 for f in files:
     subprocess.call(['midicsv', join(PATH, f), join(EXPORT_PATH, f[:-3] + "csv")])
+    temp += 1
+
+print 'converted {} files.'.format(temp)
+
 
