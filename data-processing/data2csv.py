e2b0f4a1671f88e5fce13df0e5b135d3991d96e3
Axel Nyström <mat04any@student.lth.se>
2017-05-11 18:21:53 +0200

    Structured the training sessions and csv files. Documented the data format and implemented support for different formats. Added script to copy random subset of files. Some code clean up.

--- data-processing/data2csv.py
+++ data-processing/data2csv.py
@@ -2,38 +2,53 @@
 import sys
 
 t = 0
-# Track, Time, Type, Channel, Note, Velocity
-# 2, 960, Note_on_c, 1, 81, 81
 
 
-def processChunk(chunk):
-	global t
+class Note:
 	track = "2"
-	dt = (ord(chunk[0])<<8) + (ord(chunk[1]))
-	t += dt
-	#print "chunk[0]="+str(ord(chunk[0]))+", chunk[1]="+str(ord(chunk[1]))
-	#print "dt = "+str(dt)
-	time = str(t)
-	trackType = "-"
-	if ord(chunk[2])&1 == 1:
-		trackType = "Note_on_c"
-	else:
-		trackType = "Note_off_c"
 	channel = "1"
-	note = str(ord(chunk[3]))
-	velocity = str(ord(chunk[4]))
+	velocity = "68"
+	def __init__(self, time, trackType, note):
+		self.t = time
+		self.trackType = trackType
+		self.note = note
 
-	return ", ".join((track,time,trackType,channel,note,velocity))
+	def toString(self):
+		return ", ".join((Note.track,str(self.t),self.trackType,Note.channel,self.note,Note.velocity))
+
+
+
+def processChunk(notes, chunk, datatype):
+	global t
+	
+	n = len(chunk)
+	if (datatype in ['d1'] and n != 5 or 
+			datatype in ['d2', 'd3'] and n != 6 or
+			datatype in ['d4'] and n != 7):
+		print "invalid chunk length for datatype "+datatype+": "+str(n)
+		return
+
+
+	track = "2"
+	startTimeDiff = (ord(chunk[0])<<7) + (ord(chunk[1]))
+	t += startTimeDiff
+
+	duration = (ord(chunk[2])<<7) + (ord(chunk[3]))
+
+	note = str(ord(chunk[4]))
+	notes.append(Note(t,"Note_on_c",note))
+	notes.append(Note(t+duration,"Note_off_c",note))
+
+
 
 if __name__ == '__main__':
-	
 	csvfile = 'output.csv'
-	datafile = 'output.data'
+	datafile = 'output.d1'
+	datatype = 'd1'
 	if len(sys.argv) > 2:
 		datafile = sys.argv[1]
 		csvfile = sys.argv[2]
-	
-	csv = ""
+		datatype = datafile[-2:]
 
 	header = "0, 0, Header, 1, 2, 480\n" \
 			   + "1, 0, Start_track\n" \
@@ -47,28 +62,31 @@ if __name__ == '__main__':
 				+ "2, 0, Instrument_name_t, \"Piano\"\n" \
 				+ "2, 0, Program_c, 1, 0\n"
 	
+	notes = []
+
+	words = dict()
+	wordCount = 0
 	with open(datafile) as f:
 		data = f.read()
-		n = len(data)
-		pos = 0
-		step = 5
-		while pos <= n-step:
+		for chunk in data.split(chr(128)):
+			processChunk(notes,chunk,datatype)
 
-			chunk = data[pos:pos+step]
-			pos += step
-
-			csv += processChunk(chunk) + '\n'
+		
 	
+	notes.sort(key=lambda x: x.t, reverse=False)
+	t = notes[-1].t
 	footer = "2, "+str(t)+", End_track\n" \
 				+ "0, 0, End_of_file\n"
 
 	with open(csvfile,'w') as f:
 		f.write(header)
-		f.write(csv)
+		for note in notes:
+			f.write(note.toString()+'\n')
+
 		f.write(footer)
 		print "data written to "+csvfile
-
-
+	
+	
 
 
 
