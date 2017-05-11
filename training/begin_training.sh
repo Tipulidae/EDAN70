a2c88e36140af5eaa4fc7aecb7eee5be53e717cf
Axel Nyström <mat04any@student.lth.se>
2017-05-11 15:23:26 +0200

    added training folder etc

---
+++ training/begin_training.sh
@@ -0,0 +1,11 @@
+#while true:
+#git pull origin master
+#SESSION=första raden i queue.txt
+#ta bort första raden i queue.txt
+#git commit -am "started session $SESSION"
+#git push origin master
+#username/pwd
+
+SESSION=session1
+cd $SESSION
+sh run.sh
