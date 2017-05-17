601a86ddcc0e857356e26185b11854a749679888
I am the Walrus <stag@Wal.Ross>
2017-05-17 22:01:16 +1000

    sess 4 sample

--- data-processing/sample.sh
+++ data-processing/sample.sh
@@ -6,6 +6,8 @@ DATA2CSV_PATH=~/EDAN70/data-processing/data2csv.py
 OUTPUT_PATH="${2:-sample.wav}"
 data_file=sample."${3:-d1}"
 
+echo $data_file
+
 cd $SAMPLE_PATH
 th sample.lua $1 -length 5000 > $data_file;
 
