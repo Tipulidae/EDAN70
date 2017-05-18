3465546390e4bd47d3f815e5504ecefd1c6c9230
I am the Walrus <stag@Wal.Ross>
2017-05-18 20:59:28 +1000

    refactored plot script

--- data-processing/plot.py
+++ data-processing/plot.py
@@ -1,19 +1,15 @@
+import sys
 import torchfile
-from matplotlib import pyplot as plt
 import numpy as np
 from collections import OrderedDict as od
-import sys
+from matplotlib import pyplot as plt
+
 
 if len(sys.argv) > 2:
 	path = sys.argv[1]
 	name = sys.argv[2]
 
-
 m = torchfile.load(path)
-
-#print len(m['train_losses'])
-#print m['train_losses'][10]
-#print m['val_losses']
 
 
 XT = np.linspace(0, len(m['train_losses']), len(m['train_losses']))
@@ -22,8 +18,7 @@ T = m['train_losses']
 V = od(sorted(m['val_losses'].items()))
 XV = np.fromiter(V, np.float32)
 V = np.fromiter(iter(V.values()), np.float32)
-#print XV
-#print "values: %s" % V
+
 
 fig = plt.figure()
 fig.suptitle(name)
